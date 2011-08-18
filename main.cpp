aa27fbe54abdd54a55b548226152e4e76a8debfb
tankerjedi <tankerjedi@gmail.com>
2011-08-18 21:26:42 +0200

    Test lines

--- main.cpp
+++ main.cpp
@@ -11,7 +11,24 @@ int main()
 
 mpf_set_default_prec(gmp_precision);
 
+mpf_init(ber1);
+mpf_init(ber2);
+mpf_init(eredmeny);
+ 
+mpf_set_str(ber1, "161432", 10);
+mpf_set_str(ber2, "9486321",10);
+
+
 printf("Under Construction!!!\n");
 
+fuggveny(eredmeny,ber1,ber2);
+
+gmp_printf("Az eredmény: %Ff\n",eredmeny);
+
 return 0;
+}
+
+void fuggveny(mpf_t eredmeny, mpf_t a, mpf_t b)
+{
+mpf_mul(eredmeny,a,b);
 }
--- main.h
+++ main.h
@@ -1,3 +1,10 @@
 //Parameters
 
 int gmp_precision = 1 * 1024;
+
+mpf_t ber1, ber2, eredmeny;
+
+
+//Függvények
+
+void fuggveny(mpf_t eredmeny, mpf_t a, mpf_t b);
