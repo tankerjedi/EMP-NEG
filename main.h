2f334112549c44ded5365dc9324df5ed50203094
tankerjedi <tankerjedi@gmail.com>
2011-08-31 12:25:35 +0200

    names of functions

--- main.h
+++ main.h
@@ -1,10 +1,19 @@
 //Parameters
 
+#define regiok_szama 7
+
 int gmp_precision = 1 * 1024;
 
 mpf_t ber1, ber2, eredmeny;
 
+mpf_t ber [regiok_szama];
+mpf_t arindex [regiok_szama];
 
 //Függvények
 
 void fuggveny(mpf_t eredmeny, mpf_t a, mpf_t b);
+
+void arindex_f(mpf_t eredmeny, mpf_t arindex_p [regiok_szama], mpf_t ber_p [regiok_szama] );
+
+void ber_f(mpf_t eredmeny, mpf_t arindex_p [regiok_szama], mpf_t ber_p [regiok_szama] );
+
